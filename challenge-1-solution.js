ec1c00ac8cd5f48688d6290710e2eb231522ff57
ayah <breakayah92@gmail.com>
2021-03-30 13:36:27 +0300

    min max sq

---
+++ challenge-1-solution.js
@@ -0,0 +1,15 @@
+'use strict';
+
+let array=[];
+
+function square (){
+    for (var i=0;i <array.length;i++){
+        
+            array.prototype.square=function () {
+                return Math.square.apply(this.array);
+
+                
+    console.log (square);
+        }
+    }
+}
---
+++ challenge-2-solution.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var number=prompt("1,2,7");
+
+ let array=[];
+function max (){
+  for (var i=0;i <array.length;i++){
+if (array[i]>0){
+
+  array.prototype.max=function () {
+    return Math.max.apply(this.arrays);
+
+  
+}else {
+
+
+  array.prototype.max=function (){
+    return Math.min.apply(this.arrays);
+
+    
+    console.log (min);
+
+}
+}
